135c71305501243f73a73cf0670542650a10d81c
clalil <cliljander@gmail.com>
2019-10-23 10:39:43 +0200

    Clean up schaffold further, change function into class component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <>
-    <h1>First scaffold</h1>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>First scaffold</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
