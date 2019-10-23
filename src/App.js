0004dcb598cc23b2e14f3b078621c54b595dfbc1
jonas <jonas.masharqa@gmail.com>
2019-10-23 20:59:04 +0200

    Creates ArticlesData module, articlesData state, getArticlesData function and if statement in ListArticle component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ListArticle from './Components/ListArticle'
 
 class App extends Component {
+  
   render() {
     return (
       <>
--- src/Components/ListArticle.js
+++ src/Components/ListArticle.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 
 class ListArticle extends Component {
+  state = {
+    articlesData: []
+  }
+  
+  getArticlesData() {
+    let apiUrl = 'http://localhost/3001/api/v1/articles'
+    debugger
+    this.setState({
+      articlesData: apiUrl.title
+    })
+  }
+
   render() {
+    let dataIndex;
+
+    if (this.state.articlesData === []) {
+      debugger
+      this.getArticlesData()
+    }
+    if (this.state.articlesData != []) {
+      dataIndex = (
+        <div>
+          {this.state.articlesData.map(item => {
+            return <div key={item.id}>{item.apiUrl.title}</div>
+          })}
+        </div>
+      )
+    }
     return (
       <>
         <h1>Fake News</h1>
         <div className="list-top-articles">
+          {dataIndex}
         </div>
       </>
     )
