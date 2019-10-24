b2afc538cea0ce3afaa13d44374bc41b6c5741ff
clalil <cliljander@gmail.com>
2019-10-24 13:31:29 +0200

    Change from yarn to npm. Update port.

--- cypress/integration/UserCanViewListedArticles.spec.js
+++ cypress/integration/UserCanViewListedArticles.spec.js
@@ -4,10 +4,10 @@ describe('User can view listed articles', () => {
     cy.server()
     cy.route({
       method: 'GET',
-      url: 'http://localhost:3000/api/v1/articles',
+      url: 'http://localhost:3000/v1/articles',
       response: 'fixture:list_articles.json'
     })
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3001')
 
     cy.get('h1')
       .should('contain', 'Fake News')
--- src/Modules/ArticlesData.js
+++ src/Modules/ArticlesData.js
@@ -12,8 +12,6 @@ const getArticles = async () => {
       status: 400
     }
   }
-
-
 };  
 
 export { getArticles }
