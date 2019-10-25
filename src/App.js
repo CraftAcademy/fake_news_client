564e5623dfb7f0b23bf19c8029285e03969db5c0
jonas <jonas.masharqa@gmail.com>
2019-10-25 14:33:48 +0200

    Creates LoginForm component, - adds email and password states in app - creates variable for rendering login button or form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react'
 import ListArticles from './Components/ListArticles'
 
 class App extends Component {
+  state = {
+    renderLoginForm: false,
+    email: '',
+    password: ''
+  }
+
+  renderForm = () => {
+    this.setState ({
+      renderLoginForm: !this.state.renderLoginForm
+    })
+  }
+
+  inputChangeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
   render() {
+    let renderLogin
+
+    if (this.state.renderLoginForm) {
+      renderLoginForm = (
+        <>
+          <LoginForm 
+          inputChangeHandlerProps={this.inputChangeHandler}
+          />
+        </>
+      )
+    } else {
+      <button id="login-button" onClick={this.renderForm}>Login</button>
+    }
+
     return (
       <>
+        {renderLoginForm}
+
         <ListArticles />
       </>
     )
---
+++ src/Components/LoginForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <>
+      <form>
+        
+      </form>
+        
+      </>
+    )
+  }
+}
+
+export default LoginForm
