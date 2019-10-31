6e5061fff3d0ac78064c8987bfc2259ba1c3576d
luca <l.lobacher@web.de>
2019-10-31 22:15:32 +0100

    Adds Edit button, renderEditForm function and state to SingleArticle component

--- src/Components/SingleArticle.js
+++ src/Components/SingleArticle.js
@@ -4,7 +4,8 @@ import { getSpecificArticle } from '../Modules/ArticlesData'
 class SingleArticle extends Component {
 
   state = {
-    article: null
+    article: null,
+    renderEditForm: false
   }
 
   async componentDidMount() {
@@ -18,18 +19,29 @@ class SingleArticle extends Component {
     }
   }
 
-  render() {
+  renderForm = () => {
+    this.setState({
+      renderEditForm: !this.state.renderEditForm
+    })
+  }
 
+  render() {
     let singleArticle
     const article = this.state.article
+
     if (article !== null) {
       singleArticle = (
-        <div id="single-article">
-          <p id="article-title">{article.title}</p>
-          <p id="article-content">{article.content}</p>
-        </div>
+        <>
+          <div id="single-article">
+            <p id="article-title">{article.title}</p>
+            <p id="article-content">{article.content}</p>
+          </div>
+
+          <button onClick={this.renderForm} id="edit-article">Edit</button>
+        </>
       )
     }
+
     return (
       <>
         {singleArticle}
