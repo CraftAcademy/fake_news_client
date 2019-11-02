25071fa37fb7b7e07449e71fbdc2f4c0a39b01b9
jonas <jonas.masharqa@gmail.com>
2019-11-02 14:33:46 +0100

    Creates integration test for user can sign up.

---
+++ cypress/integration/UserCanSignupToApplication.spec.js
@@ -0,0 +1,25 @@
+describe('User can sign up to application', () => {
+  it('successfully signs up', () => {
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/v1/auth',
+      response: 'fixture:successful_user_login.json',
+      status: 200,
+      headers: {
+        'uid': 'user@mail.com'
+      }
+    })
+
+    cy.visit('http://localhost:3001')
+
+    cy.get('#signup-button').click()
+    cy.get('#signup-form').within(() => {
+      cy.get('#email-input').type('user@mail.com')
+      cy.get('#password-input').type('password')
+      cy.get('#password-confirmation').type('password')
+    })
+    cy.get('#submit-signup-form').click()
+    cy.get('#welcome-message').should('contain', 'Hello user@mail.com')
+  })
+});
