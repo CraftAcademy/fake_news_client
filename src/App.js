1530330ec8d2e5bd6e5c3eb54fc8b1cceb284315
jonas <jonas.masharqa@gmail.com>
2019-11-03 17:53:54 +0100

    Adds react router package, wraps App in router and imports switch and route.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ListArticles from './Components/ListArticles'
 import Login from './Components/Login'
 import CreateArticle from './Components/CreateArticle'
+import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import "semantic-ui-css/semantic.min.css"
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import configureStore from './state/store/configureStore'
+import { BrowserRouter } from 'react-router-dom'
 
 const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 document.getElementById('root'))
 
