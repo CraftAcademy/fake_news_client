8b0c093cd8eb04c29d5b2f44246d35a8d574db83
clalil <cliljander@gmail.com>
2019-11-04 15:42:24 +0100

    Solve semaphore error after merge from upstream.

--- cypress/integration/UserCanEditArticle.spec.js
+++ cypress/integration/UserCanEditArticle.spec.js
@@ -10,11 +10,26 @@ describe('User can edit an article', () => {
       method: "GET",
       url: "http://localhost:3000/v1/articles/1",
       response: "fixture:successfully_view_article.json"
+    }),
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/v1/auth/sign_in',
+      response: 'fixture:successful_user_login.json',
+      status: 200
     })
     cy.visit('http://localhost:3001/')
+
+    cy.get('#login-button').click()
+    cy.get('#login-form').within(() => {
+      cy.get('#email-input').type('user@mail.com')
+      cy.get('#password-input').type('password')
+    })
+    cy.get('#submit-login-form').click()
+    cy.get('#welcome-message').should('contain', 'Hello user@mail.com')
+
   })
 
-  it('successfully edits an article', () => {
+  it('can successfully edit an article if logged in', () => {
     cy.route({
       method: 'PUT',
       url: 'http://localhost:3000/v1/articles',
@@ -39,7 +54,7 @@ describe('User can edit an article', () => {
       .should('contain', 'Article was successfully edited')
   })
 
-  it('cannot edit an article', () => {
+  it('cannot edit an article unless all input is valid', () => {
     cy.route({
       method: 'PUT',
       url: 'http://localhost:3000/v1/articles',
