9ae19dfd54fcabb45a250f37e4b4e6cf8194bb2e
clalil <cliljander@gmail.com>
2019-11-05 15:41:47 +0100

    Add all sad paths, extract signup to cypress module. Debug.

--- cypress/integration/LoggedInUserCanBuyASubscription.spec.js
+++ cypress/integration/LoggedInUserCanBuyASubscription.spec.js
@@ -9,27 +9,18 @@ describe('User can buy a subscription for the articles', () => {
       cy.visit('http://localhost:3001')
   })
 
-  it('successfully signs up and sees a subscription option', () => {
-    cy.route({
-      method: 'POST',
-      url: 'http://localhost:3000/v1/auth',
-      response: 'fixture:successful_user_signup.json',
-      status: 200
-    }),
+  it('successfully signs up and enters the valid credentials', () => {
     cy.route({
       method: 'POST',
       url: 'http://localhost:3000/v1/payments',
       response: 'fixture:successful_payment.json',
       status: 200
     })
-
-    cy.get('#signup-button').click()
-    cy.get('#signup-form').within(() => {
-      cy.get('#email-input').type('user@mail.com')
-      cy.get('#password-input').type('password')
-      cy.get('#password-confirmation').type('password')
-    })
-    cy.get('#submit-signup-form').click()
+    cy.signup_user(
+      "user@mail.com",
+      "password",
+      "password"
+    );
     cy.get('#welcome-message').should('contain', 'Hello user@mail.com')
 
     cy.get('#payment-form').should('contain', 'Please select a subscription plan:')
@@ -54,7 +45,87 @@ describe('User can buy a subscription for the articles', () => {
     });
     cy.get("#submit-payment")
       .click()
-    cy.get('#successful-payment')
+    cy.get('#payment-feedback-message')
       .should('contain', 'Transaction successful')
+  })
+
+  it('successfully signs up but enters invalid credentials', () => {
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/v1/payments',
+      response: 'fixture:successful_payment.json',
+      status: 402
+    })
+    cy.signup_user(
+      "user@mail.com",
+      "password",
+      "password"
+    );
+    cy.get('#welcome-message').should('contain', 'Hello user@mail.com')
+
+    cy.get('#payment-form').should('contain', 'Please select a subscription plan:')
+    cy.wait(2000);
+    cy.get('iframe[name^="__privateStripeFrame5"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cardnumber"]')
+        .type("4242", { delay: 10 });
+    });
+    cy.get('iframe[name^="__privateStripeFrame6"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="exp-date"]')
+        .type("1222");
+    });
+    cy.get('iframe[name^="__privateStripeFrame7"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cvc"]')
+        .type("223");
+    });
+    cy.get("#submit-payment")
+      .click()
+    cy.get('#invalid-credentials')
+      .should('contain', 'Something went wrong, please try again')
+  })
+
+  it('successfully signs up but the subscription fails on the server side', () => {
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/v1/payments',
+      response: 'fixture:unsuccessful_payment.json',
+      status: 402
+    })
+    cy.signup_user(
+      "user@mail.com",
+      "password",
+      "password"
+    );
+    cy.get('#welcome-message').should('contain', 'Hello user@mail.com')
+
+    cy.get('#payment-form').should('contain', 'Please select a subscription plan:')
+    cy.wait(2000);
+    cy.get('iframe[name^="__privateStripeFrame5"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cardnumber"]')
+        .type("42424242424242424242", { delay: 10 });
+    });
+    cy.get('iframe[name^="__privateStripeFrame6"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="exp-date"]')
+        .type("1222");
+    });
+    cy.get('iframe[name^="__privateStripeFrame7"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cvc"]')
+        .type("223");
+    });
+    cy.get("#submit-payment")
+      .click()
+    cy.get('#payment-feedback-message')
+      .should('contain', 'Transaction unsuccessful')
   })
 })
--- src/Components/CheckOutForm.js
+++ src/Components/CheckOutForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Button, Form, Card } from 'semantic-ui-react'
+import { Button, Card } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import getCredentials from '../Modules/GetCredentials'
 import {
@@ -24,68 +24,64 @@ class CheckOutForm extends Component {
       if (token) {
         this.payment(token.id)
       } else {
-        debugger;
         this.setState({ error: true, loading: false })
       }
     })
   }
 
-payment = async stripeToken => {
-  try {
-    let response = await axios.post('http://localhost:3000/v1/payments', {
-      stripeToken
-    },
-      { headers: getCredentials() }
-    );
-    if (response.status === 200) {
-      debugger;
-      this.setState({ responseMessage: response.data.message })
-      debugger;
+  payment = async stripeToken => {
+    try {
+      let response = await axios.post('http://localhost:3000/v1/payments', {
+        stripeToken
+      },
+        { headers: getCredentials() }
+      );
+      if (response.status === 200) {
+        this.setState({ responseMessage: response.data.message })
+      }
+    } catch (error) {
+      this.setState({ loading: false })
+      this.setState({ responseMessage: error.response.data.message })
     }
-  } catch (error) {
-    debugger;
-    this.setState({ loading: false })
-    this.setState({ responseMessage: error.response.data.message })
-  }
-};
+  };
 
-render() {
-  let renderSubscribeForm
-  let error
+  render() {
+    let renderSubscribeForm
+    let error
 
-  if (this.props.currentUser.isSignedIn) {
-    renderSubscribeForm = (
-      <div id="payment-form">
-        <label>Please select a subscription plan:</label>
-        <Card>
-          <Card.Content header="Yearly" />
-          <Card.Content description="1000 SEK" />
-        </Card>
-        <label>Credit card number:</label>
-        <div id="card-number-element"><CardNumberElement /></div>
-        <label>Expiration date:</label>
-        <CardExpiryElement />
-        <label>CVC:</label>
-        <CardCVCElement />
-        <Button onClick={this.payWithStripe} id="submit-payment">
-          Submit Payment
+    if (this.props.currentUser.isSignedIn) {
+      renderSubscribeForm = (
+        <div id="payment-form">
+          <label>Please select a subscription plan:</label>
+          <Card>
+            <Card.Content header="Yearly" />
+            <Card.Content description="1000 SEK" />
+          </Card>
+          <label>Credit card number:</label>
+          <div id="card-number-element"><CardNumberElement /></div>
+          <label>Expiration date:</label>
+          <CardExpiryElement />
+          <label>CVC:</label>
+          <CardCVCElement />
+          <Button onClick={this.payWithStripe} id="submit-payment">
+            Submit Payment
           </Button>
+        </div>
+      )
+    }
+
+    if (this.state.error) {
+      error = <p id="invalid-credentials">Something went wrong, please try again</p>
+    }
+
+    return (
+      <div>
+        {renderSubscribeForm}
+        <p id="payment-feedback-message">{this.state.responseMessage}</p>
+        {error}
       </div>
     )
   }
-
-  if (this.state.error) {
-    error = <p id="unsuccessful-payment">Something went wrong, please try again</p>
-  }
-
-  return (
-    <div>
-      {renderSubscribeForm}
-      <p id="successful-payment">{this.state.responseMessage}</p>
-      {error}
-    </div>
-  )
-}
 }
 
 const mapStateToProps = state => {
