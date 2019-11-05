33e1b8e32b6734e603ee3b96d7676a92a2182f31
luca <l.lobacher@web.de>
2019-11-05 10:52:10 +0100

    Modifies navbar to not show login and signup if user isSignedIn

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = ({currentUser}) => {
   return (
     <>
       <nav id="navbar">
         <NavLink id='nav-home' to='/'>Home</NavLink>
-        <NavLink id='nav-login' to='/login'>Login</NavLink>
-        <NavLink id='nav-signup' to='/signup'>Signup</NavLink>
+        {currentUser.isSignedIn === false && (
+          <NavLink id='nav-login' to='/login'>
+            Login
+          </NavLink>
+        )}
+        {currentUser.isSignedIn === false && (
+          <NavLink id='nav-signup' to='/signup'>
+            Signup
+          </NavLink>
+        )}
         <NavLink id='nav-create' to='/create'>Create Article</NavLink>
       </nav>
     </>
   )
 }
 
-export default Navbar
+const mapStateToProps = state => {
+  return {
+    currentUser: state.reduxTokenAuth.currentUser
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(Navbar)
