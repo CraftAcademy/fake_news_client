072458ca0571df5865b82337f7f71ce00039f118
clalil <cliljander@gmail.com>
2019-11-06 14:35:43 +0100

    Add translation to error modal.

--- src/Components/AlertModal.js
+++ src/Components/AlertModal.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Message } from 'semantic-ui-react'
 import FlashMessage from 'react-flash-message'
+import { useTranslation } from 'react-i18next'
 
 const AlertModal = () => {
+  const { t } = useTranslation()
+
   return (
   <FlashMessage duration={5000} >
-    <Message id="flash-message">You need to be logged in to view this article.</Message>
+    <Message id="flash-message">{t("flash.alert")}</Message>
   </FlashMessage>
   )
 }
