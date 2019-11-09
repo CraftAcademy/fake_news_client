03e7bf2ba00ef660b125511561bebd1fb608572c
clalil <cliljander@gmail.com>
2019-11-09 19:08:06 +0100

    Remove un-used code.

--- src/Components/Categories/Politics.js
+++ src/Components/Categories/Politics.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import { getArticles, getCategories } from '../../Modules/ArticlesData'
+import { getArticles } from '../../Modules/ArticlesData'
 import { Image } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom'
 
 class Politics extends Component {
   state = {
     articles: [],
-    categories: [],
     categoryName: 'Politics'
   }
 
@@ -16,34 +15,19 @@ class Politics extends Component {
     })
   }
 
-  async componentDidMount() {
-    let categories = await getCategories()
-    this.setState({
-      categories: categories
-    })
+  componentDidMount() {
     this.getArticlesData()
   }
 
   async getArticlesData() {
-    debugger
     let fetch = await getArticles();
     if (fetch.error) {
       this.setErrorMessage(fetch.error)
     } else {
-      debugger
       this.setState({
         articles: fetch
       })
     }
-  }
-
-  renderArticles(article) {
-    return (
-      <NavLink id={`article_${article.id}`} key={article.id} to={`/article/${article.id}`} >
-        <Image src={article.image} alt="" wrapped ui={false} />
-        <h3>{article.title}</h3>
-      </NavLink>
-    )
   }
 
   render() {
@@ -53,11 +37,8 @@ class Politics extends Component {
     let politicalArticles
 
     articles.forEach(article => {
-      debugger
       if (article.category.name === categoryName) {
         return filteredArticleList.push(article)
-      } else {
-        errorMessage = <p>There are no Articles in this category</p>
       }
     })
 
